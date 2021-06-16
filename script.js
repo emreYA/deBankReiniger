e8e94762ae0dc985ef879781eceb0c55a752b9ca
emreYA <emreYA@users.noreply.github.com>
2021-06-16 13:36:20 +0200

    start of footer
    start of footer

--- script.js
+++ script.js
@@ -2,9 +2,29 @@ new Glider(document.querySelector('.glider'), {
   slidesToShow: 1.04,
   slidesToScroll: 1,
   draggable: true,
+  scrollLock: true,
   dots: '.dots',
   arrows: {
     prev: '.glider-prev',
     next: '.glider-next'
-  }
+  },
+  responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 'auto',
+        slidesToScroll: 'auto',
+      }
+    },{
+      // screens greater than >= 992px
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2.05,
+        slidesToScroll: 1,
+        scrollLock: false,
+      }
+    }
+  ]
 });
